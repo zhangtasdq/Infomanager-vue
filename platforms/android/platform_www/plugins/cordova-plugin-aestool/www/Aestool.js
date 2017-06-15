5530970e0d7c23297c449539d7d96feb46e05cfd
zhang <cqmygasdq@126.com>
2017-06-15 22:48:57 +0800

    init

---
+++ platforms/android/platform_www/plugins/cordova-plugin-aestool/www/Aestool.js
@@ -0,0 +1,30 @@
+cordova.define("cordova-plugin-aestool.Aestool", function(require, exports, module) {
+var exec = require('cordova/exec');
+
+exports.decrypt = function(password, content, callback) {
+    exec(function(content) {
+             callback(null, content)
+         },
+         function(error) {
+             callback(error)
+         },
+         "Aestool",
+         "decrypt",
+         [password, content]
+     );
+}
+
+exports.encrypt = function (password, content, callback) {
+    exec(function(content) {
+             callback(null, content)
+         },
+         function(error) {
+             callback(error)
+         },
+         "Aestool",
+         "encrypt",
+         [password, content]
+     );
+}
+
+});
